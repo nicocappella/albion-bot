8bd014038743622274f1f2238ba0297ccb5fd6bf
Nicolas Cappella <nicolascappellabb@gmail.com>
2025-10-16 10:17:41 -0300

    fix command

--- src/bot/commands/guild/dto/event.dto.ts
+++ src/bot/commands/guild/dto/event.dto.ts
@@ -6,22 +6,24 @@ export class CrearEventoDto {
     description: 'Lugar del evento',
     required: true,
     choices: [
-      { name: 'Bridgewatch Portal', value: 'brigdewatch-portal' },
-      { name: 'Arthur Rest', value: 'arthur-rest' },
-      { name: 'HO Frontspring Volcano', value: 'ho-frontspring-volcano' },
+      { name: 'Bridgewatch Portal', value: 'Bridgewatch Portal' },
+      { name: 'Arthur Rest', value: 'Arthur Rest' },
+      { name: 'HO Frontspring Volcano', value: 'HO Frontspring Volcano' },
     ],
   })
   lugar!: string;
-  @StringOption({
-    name: 'dia',
-    description: 'Día o desplazamiento (hoy, mañana, lunes, +2, etc.)',
-    required: true,
-  })
-  dia?: string;
+
   @StringOption({
     name: 'hora_utc',
     description: 'Horario en formato HH:mm (UTC)',
     required: true,
   })
   horaUtc!: string;
+
+  @StringOption({
+    name: 'dia',
+    description: 'Día o desplazamiento (0 = hoy, 1 = mañana)',
+    required: false,
+  })
+  dia?: string;
 }
